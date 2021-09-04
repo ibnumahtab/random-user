2035b0ee35bc5380e7ef290adf55908e6be74c73
ibnumahtab <ibnumahtab@gmail.com>
2021-09-04 20:54:35 +0600

    UI Complete

--- random-user.js
+++ random-user.js
@@ -1,17 +1,24 @@
-// CatchUp with ID
-const userContainer = document.getElementById('user-container');
-
 // Load User Data
 const loadUser = async () => {
     const url = `https://randomuser.me/api/?gender=male`;
     const res = await fetch(url);
     const data = await res.json();
-    displayUser(data.results);
+    displayUser(data.results[0]);
 };
 
-loadUser();
+// Set Inner Text
+const setInnerText = (setItem) => {
+    const id = document.getElementById('name');
+    id.innerText = setItem;
+};
 
 // Displaying Loading Data
 const displayUser = user => {
-    
-}
+    console.log(user);
+    setInnerText(`${user.name.title} ${user.name.first} ${user.name.last}`);
+};
+
+const mouseOverEvent = iconId => {
+    const id = document.getElementById(iconId);
+    id.addEventListener('mouseover',);
+};
