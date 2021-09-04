48d53241b6698eccd63e7f5ed62de58188706e6e
ibnumahtab <ibnumahtab@gmail.com>
2021-09-05 00:27:29 +0600

    Finish The Project

--- random-user.js
+++ random-user.js
@@ -1,3 +1,14 @@
+// My Info in Object
+let globalObj = {
+    email: 'ibnumahtab@gmail.com',
+    cell: '+880162012345',
+    dob: {
+        date: '08 March 1998',
+        age: 23,
+    },
+    gender: 'Male',
+};
+
 // Load User Data
 const loadUser = async () => {
     const url = `https://randomuser.me/api/?gender=male`;
@@ -6,19 +17,36 @@ const loadUser = async () => {
     displayUser(data.results[0]);
 };
 
+// Displaying Loading Data
+const displayUser = user => {
+    globalObj = user;
+    setInnerText('name', `${user.name.title} ${user.name.first} ${user.name.last}`);
+    document.getElementById('user-image').setAttribute('src', `${user.picture.large}`);
+};
+
 // Set Inner Text
-const setInnerText = (setItem) => {
-    const id = document.getElementById('name');
+const setInnerText = (fieldID, setItem) => {
+    const id = document.getElementById(fieldID);
     id.innerText = setItem;
 };
 
-// Displaying Loading Data
-const displayUser = user => {
-    console.log(user);
-    setInnerText(`${user.name.title} ${user.name.first} ${user.name.last}`);
-};
-
+// Showing Others Info with Mouseover Event
 const mouseOverEvent = iconId => {
     const id = document.getElementById(iconId);
-    id.addEventListener('mouseover',);
+    id.addEventListener('mouseover', () => {
+        let objProperty = globalObj[iconId];
+        if (iconId === 'age') {
+            objProperty = globalObj.dob.age;
+        } else if (iconId === 'date') {
+            objProperty = globalObj.dob.date;
+        }
+        setInnerText('info', objProperty)
+    });
 };
+
+// Call The Mouseover Event Function
+mouseOverEvent('email');
+mouseOverEvent('cell');
+mouseOverEvent('age');
+mouseOverEvent('date');
+mouseOverEvent('gender');
